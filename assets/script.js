6f672348fa4fcb521ceb36f0fa418808c2a62a64
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-08 16:09:16 +0200

    add the bonus assignment as a comment

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,12 @@ console.log('test');
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
+// BONUS
+// Facciamo accendi e spegni:
+// Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+// Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+// E così via...
+
 // button element
 const buttonEl = document.getElementById('button_light')
 console.log(buttonEl);
