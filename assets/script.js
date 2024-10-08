5c5908563294088a86ab83269547e32ca3aa26fa
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-08 15:50:49 +0200

    complete the assigment

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,14 @@ console.log('test');
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
+const buttonEl = document.getElementById('button_light')
+console.log(buttonEl);
+
+const imgOnEl = document.getElementById('light_on')
+const imgOffEl = document.getElementById('light_off')
+console.log(imgOnEl , imgOffEl);
+
+buttonEl.addEventListener('click', () => {
+    imgOffEl.style.display = 'inline';
+    imgOnEl.style.display = 'none';
+})
